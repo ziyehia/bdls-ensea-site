bf394807529dfbc3f8021aa07d3fe9af559f0bc9
Yehia Zeaiter <yehyazeaiter48@gmail.com>
2025-12-24 23:03:01 +0100

    Update docusaurus.config.js with url to ziyehia.github.io/bdls/

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://bdls.github.io',
+  url: 'https://ziyehia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bdls/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
