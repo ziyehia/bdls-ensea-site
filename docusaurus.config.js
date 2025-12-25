5448aee351a3538d4db864f9dee110e049cdf8a4
Yehia Zeaiter <yehyazeaiter48@gmail.com>
2025-12-25 23:49:14 +0100

    Remove dark color mode switch in docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
-        respectPrefersColorScheme: true,
+        defaultMode: 'light',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: 'Acceuil',
