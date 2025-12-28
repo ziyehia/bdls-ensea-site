6ec190a6ed5be513ede1e99311f84534a883963a
Yehia Zeaiter <yehyazeaiter48@gmail.com>
2025-12-28 20:38:13 +0100

    added social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/bdls_social_card.jpg',
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true,
